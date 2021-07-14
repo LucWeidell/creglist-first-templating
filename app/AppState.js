b333f92bd3b22f21355cfc700710f3cbfe5868a1
W1ndyWalker <60443786+W1ndyWalker@users.noreply.github.com>
2021-07-14 16:39:20 -0600

    house and jobs html and funct. stucture

--- app/AppState.js
+++ app/AppState.js
@@ -1,4 +1,5 @@
 import Car from "./Models/Car.js"
+import House from "./Models/House.js"
 import { EventEmitter } from "./Utils/EventEmitter.js"
 import { isValidProp } from "./Utils/isValidProp.js"
 
@@ -6,8 +7,12 @@ class AppState extends EventEmitter {
   // @type {Value[]} */
   //values = []
   /** @type {Car[]} */
-  cars = [new Car({make: 'Ford', model: 'Pinto', year: 1987, price: 1200, description: 'This Car is HOT!', imgURL: '//placebeard.it/750x547'}),
-  new Car({make: 'VW', model: 'Gremlin', year: 1988, price: 3400, description: 'Lime Green! You gonna love it', imgURL:'//placebeard.it/750x547'})]
+  cars = [
+    new Car({make: 'Ford', model: 'Pinto', year: 1987, price: 1200, description: 'This Car is HOT!', imgURL: '//placebeard.it/750x547'}),
+    new Car({make: 'VW', model: 'Gremlin', year: 1988, price: 3400, description: 'Lime Green! You gonna love it', imgURL:'//placebeard.it/750x547'})]
+  houses = [
+    new House
+  ]
 }
 
 export const ProxyState = new Proxy(new AppState(), {
